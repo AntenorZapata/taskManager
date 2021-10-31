fc087b75d61774a5cab2750024eaef582f17a8f1
Antenor <antenorzapata@gmail.com>
2021-10-30 21:02:15 -0300

    test: should return an non-empty array of objects with the properties 'author', 'task' and 'category'

--- server/src/tests/models/taskModel.test.js
+++ server/src/tests/models/taskModel.test.js
@@ -14,4 +14,26 @@ describe('Get all tasks', () => {
       expect(response.length).to.be.equal(0);
     });
   });
+
+  describe('when there is at least one task', async () => {
+    it('should return an array', async () => {
+      const response = await taskModel.getAll();
+      expect(response).to.be.an('array');
+    });
+
+    it('should return a non-empty array', async () => {
+      const response = await taskModel.getAll();
+      expect(response.length).greaterThan(0);
+    });
+
+    it('should return an array of objects', async () => {
+      const [task] = await taskModel.getAll();
+      expect(task).to.be.an('object');
+    });
+
+    it("should return an array of objects with the properties 'author', 'task', 'category'", async () => {
+      const [task] = await taskModel.getAll();
+      expect(task).to.include.all.keys('author', 'task', 'category');
+    });
+  });
 });
