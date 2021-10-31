825aa474f19af291446205b0fb5442e943587d3d
Antenor <antenorzapata@gmail.com>
2021-10-31 19:15:56 -0300

    test: controller - should return a 200 HTTP status

--- server/src/tests/controller/taskController.test.js
+++ server/src/tests/controller/taskController.test.js
@@ -2,3 +2,28 @@ const { expect } = require('chai');
 const sinon = require('sinon');
 const taskController = require('../../api/controllers');
 const taskService = require('../../api/services');
+
+describe('Get all tasks in the DB', () => {
+  describe('When there is no task in the DB', async () => {
+    const request = {};
+    const response = {};
+
+    before(() => {
+      sinon.stub(taskService, 'getAll').resolves([]);
+
+      request.body = {};
+
+      response.status = sinon.stub().returns(response);
+      response.json = sinon.stub().returns(response);
+    });
+
+    after(() => {
+      taskService.getAll.restore();
+    });
+
+    it('should return a 200 HTTP status', async () => {
+      await taskController.getAll(request, response);
+      expect(response.status.calledWith(200)).to.be.eq(true);
+    });
+  });
+});
