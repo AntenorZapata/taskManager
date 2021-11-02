fde51a3d693751271b4b84a8c059aa1459609ba7
Antenor <antenorzapata@gmail.com>
2021-11-02 17:03:41 -0300

    feat: render the table in different parts

--- client/src/components/Table/Table.jsx
+++ client/src/components/Table/Table.jsx
@@ -1,70 +1,132 @@
 import React from 'react';
+import { RiDeleteBin2Fill } from 'react-icons/ri';
+import { IoFilterSharp } from 'react-icons/io5';
+import { AiTwotoneEdit } from 'react-icons/ai';
+import { useHistory } from 'react-router-dom';
 import titles from '../../utils/tableTitles';
 import './Table.css';
+import sort from '../../asserts/sort.png';
 
 function Table({ tasks }) {
+  const history = useHistory();
+
+  const handleHistory = (id) => {
+    history.push(`/${id}`);
+  };
+
+  const handleStatus = (e) => {
+
+  };
+
+  const handleTableContent = (el) => (
+    <tr key={el.id}>
+      <td data-col-title="ID" className="task" onClick={() => handleHistory(el.id)}>{el.id}</td>
+      <td data-col-title="Categoria">{el.category}</td>
+      <td data-col-title="Tarefa">{el.task}</td>
+      <td data-col-title="Usuário">{el.author}</td>
+      <td data-col-title="Status">
+        <select name="" id="" onClick={handleStatus}>
+          <option value={el.status} defaultValue>{el.status}</option>
+          <option value="inProgress">Em andamento</option>
+          <option value="paused">Pausado</option>
+          <option value="done">Pronto</option>
+        </select>
+
+      </td>
+      <td data-col-title="Data">{el.createAt}</td>
+      <td>
+        <AiTwotoneEdit
+          className="table-icon"
+                  // onClick={() => handleEditTable(gt._id,
+                  //   guitarTable, setState, state, initialState)}
+          type="button"
+        />
+      </td>
+      <td>
+        <RiDeleteBin2Fill
+          className="table-icon remove-table-icon"
+                  // onClick={() => handleDeleteRow(gt._id, {
+                  //   setState, initialState, page, setPage,
+                  // }, paginate(valueFilter, 5))}
+          type="button"
+        />
+
+      </td>
+    </tr>
+  );
+
+  const handleTableHead = () => (
+    <table
+      rules="none"
+      border="1"
+      className="smart-table"
+    >
+      <thead>
+        <tr>
+          {titles.map((title) => (
+            <th
+              key={title.id}
+              name={title.state}
+            >
+              {title.value}
+            </th>
+          ))}
+          <th>
+            <IoFilterSharp
+              className="table-icon filter-icon"
+              type="button"
+            />
+          </th>
+          <th className="sort-warning">
+            <div className="msg-popup">
+              Clique nos títulos para ordernar a tabela.
+            </div>
+            <img src={sort} alt="sort" />
+
+          </th>
+        </tr>
+
+      </thead>
+      <tbody>
+        {/* {valueFilter && valueFilter.length > 0
+              ? (paginate(valueFilter, 5)[page].map((gt) => ( */}
+        {tasks
+          && (tasks.map((el) => (
+            handleTableContent(el)
+          ))
+
+          )}
+        {/* ))) : <tr><td>Not found</td></tr> } */}
+      </tbody>
+    </table>
+  );
+
+  const handleBtnsTable = () => (
+    <div className="btns-table">
+      <button
+            // disabled={page === paginate(valueFilter, 5).length - 1}
+            // onClick={handlePagination}
+        type="button"
+        name="next"
+      >
+        Proximo
+      </button>
+      {/* {page > 0 && ( */}
+      <button
+        type="button"
+        name="back"
+      >
+        Voltar
+      </button>
+      {/* )} */}
+    </div>
+  );
+
   return (
     <div className="responsive-container">
-      <table
-        rules="none"
-        border="1"
-        className="smart-table"
-      >
-        <thead>
-          <tr>
-            {titles.map((title) => (
-              <th
-                key={title.id}
-                name={title.state}
-              >
-                {title.value}
-              </th>
-            ))}
+      {handleTableHead()}
+      {handleBtnsTable()}
 
-            <th className="sort-warning">
-              <div className="msg-popup">
-                Clique nos títulos para ordernar a tabela.
-              </div>
-            </th>
-          </tr>
-          <tr>
-            <th>
-              <input
-                className="table-filters-input"
-                type="text"
-              />
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {tasks
-          && (tasks.map((el) => (
-            <tr>
-              <td data-col-title="ID">{el.id}</td>
-              <td data-col-title="Categoria">{el.category}</td>
-              <td data-col-title="Tarefa">{el.task}</td>
-              <td data-col-title="Usuário">{el.author}</td>
-              <td data-col-title="Status">{el.status}</td>
-              <td data-col-title="Data">{el.createAt}</td>
-            </tr>
-          ))
-          )}
-        </tbody>
-      </table>
-      <div className="btns-table">
-        <button
-          type="button"
-          name="next"
-        >
-          Proximo
-        </button>
-        <button
-          type="button"
-          name="back"
-        >
-          Voltar
-        </button>
-      </div>
     </div>
   );
 }
--- client/src/utils/tableTitles.js
+++ client/src/utils/tableTitles.js
@@ -3,7 +3,7 @@ const titles = [
   { id: 2, value: 'Categoria', state: 'category' },
   { id: 4, value: 'Tarefa', state: 'task' },
   { id: 3, value: 'Usuário', state: 'author' },
-  { id: 5, value: 'Status', state: { inProgress: 'Em andamento', paused: 'Pendente', done: 'Pronto' } },
+  { id: 5, value: 'Status', state: [{ inProgress: 'Em andamento' }, { paused: 'Pendente' }, { done: 'Pronto' }] },
   { id: 6, value: 'Data', state: 'createAt' },
 
 ];
